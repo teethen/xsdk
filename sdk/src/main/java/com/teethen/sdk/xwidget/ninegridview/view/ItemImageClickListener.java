c2f1e91b88365bd82eae96bbb798f0803f99963b
xingqingxiang <wanda360@qq.com>
2018-03-02 10:35:12 +0800

    add ninegridview demo

--- sdk/src/main/java/com/teethen/sdk/xwidget/ninegridview/view/ItemImageClickListener.java
+++
@@ -1,9 +0,0 @@
-package com.teethen.sdk.xwidget.ninegridview;
-
-import android.content.Context;
-import android.widget.ImageView;
-import java.util.List;
-
-public interface ItemImageClickListener<T> {
-    void onItemImageClick(Context context, ImageView imageView, int index, List<T> list);
-}
